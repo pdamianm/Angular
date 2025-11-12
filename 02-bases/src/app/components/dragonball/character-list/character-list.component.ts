e7d860241a5ac02e0a63ad8652f2dd8dd8d814d0
Damián Muñoz <pikimd14@gmail.com>
2025-11-12 19:40:09 -0300

    Convertir submódulos en carpetas normales

---
+++ 02-bases/src/app/components/dragonball/character-list/character-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import type { Character } from '../../../interfaces/character.interfaces';
+
+@Component({
+  selector: 'dragonball-character-list',
+  templateUrl: './character-list.component.html',
+})
+export class CharacterListComponent {
+
+  characters = input.required<Character[]>();
+  listName = input.required<string>();
+
+}
---
+++ 03-gifs-app/src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+
+  prodution: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+
+  //API Keys-URLS
+  giphyApiKey: 'p0DYVU2kQkBMq95ZnGDdwBAGh1mlH7gB',
+  giphyUrl: 'https://api.giphy.com/v1',
+
+};
